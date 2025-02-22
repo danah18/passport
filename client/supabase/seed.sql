6500ab98164a225f440273cf1221b271d649579c
Kit Zellerbach <kit.zeller@gmail.com>
2025-02-22 03:57:09 -0500

    Basic supabase postgis example w/ google maps

---
+++ client/supabase/seed.sql
@@ -0,0 +1,6 @@
+  insert into public.restaurants
+  (name, location)
+values
+  ('Supa Burger', gis.st_point(-73.946823, 40.807416)),
+  ('Supa Pizza', gis.st_point(-73.94581, 40.807475)),
+  ('Supa Taco', gis.st_point(-73.945826, 40.80629));
