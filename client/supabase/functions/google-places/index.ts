0e186b28b0c4d080b7283aae1da7c0b0310e0977
danah18 <eastlabs8@gmail.com>
2025-03-04 21:14:07 -0800

    function works

--- client/supabase/functions/google-places/index.ts
+++ client/supabase/functions/google-places/index.ts
@@ -5,28 +5,52 @@
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 
-console.log("Hello from Functions")
+// Import the PlacesClient using ES module syntax
+import { PlacesClient } from "googleplaces";
+
+// // Instantiates a client
+const placesClient = new PlacesClient();
 
 Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!!!!`,
-  }
+  try {
+    const requestUrl = new URL(req.url);
+    const placeId = requestUrl.searchParams.get("placeId");
+    
+    const url = `https://places.googleapis.com/v1/places/${placeId}`;
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": "AIzaSyCc3qVwBWVG5WUPkeLfcIV5NE9DdakgglQ",
+        "X-Goog-FieldMask": "displayName,formattedAddress,rating,userRatingCount,googleMapsUri,photos,reviews" 
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+      const data = await response.json();
+
+      return new Response(
+        JSON.stringify(data),
+        { headers: { "Content-Type": "application/json" } },
+      )
+  } catch (error) {
+      console.error("Fetch error:", error);
+      throw error;
+  }
 })
 
+    
+// // place details search
+// // photo details with place details info
+
+// Fix usage of API key
+
 /* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/google-places' \
+curl -i --location --request GET 'http://127.0.0.1:54321/functions/v1/google-places?placeId=ChIJj61dQgK6j4AR4GeTYWZsKWw' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
+    --header 'Content-Type: application/json' 
 */
